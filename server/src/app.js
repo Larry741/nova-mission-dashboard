7a1896a4d6837d944bf8a19afaf0c1ad4bc0a3b5
Larry741 <85595046+Larry741@users.noreply.github.com>
2022-05-29 09:19:41 +0100

    app

---
+++ server/src/app.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const helmet = require("helmet"); 
+
+const v1 = require('./routes/v1')
+
+const app = express();
+
+app.use(helmet());
+app.use(cors({
+  origin: 'https://localhost:8000',
+}))
+app.use(morgan('combined'));
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '..',  'public')));
+
+app.use('/v1', v1);
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..',  'public', 'index.html'));
+})
+
+module.exports = app;
